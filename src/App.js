96a15df21a19c314321db258b155d4f8e5a7b15c
David Gonnzalez <davidgonzalez_1999@yahoo.com>
2023-12-17 21:00:05 -0800

    working on UI of recipe finder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import styled from "styled-components"
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
+const Header = styled.div`
+  color: white;
+  background-color: black;
+  display: flex;
+  flex-direction: row;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px #555;
+  align-items: center;
+`
+const AppNameComponent = styled.div`
+display: flex;
+align-items: center;
+`
+const AppIcon = styled.img`
+width: 36px;
+height: 36px;
+margin: 15px;
+`
+
+
+const SearchComponent = styled.div``
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <div className="App">
+    <Container>
+      <Header>
+        <AppNameComponent>
+          <AppIcon src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Rpb_food_icon.svg/1200px-Rpb_food_icon.svg.png"/> Foodie
+        </AppNameComponent>
+        <SearchComponent>
+          <img src="/search-icon.svg" alt="search icon" style={{color: "blue"}} />
+        </SearchComponent>
+      </Header>
+    </Container>
+
+    // </div>
+  )
 }
 
-export default App;
+export default App
