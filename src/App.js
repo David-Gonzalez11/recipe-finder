0ca8162ea1286a70119552315b90ac97da78783e
David Gonnzalez <davidgonzalez_1999@yahoo.com>
2023-12-23 08:38:27 -0800

    recipe done, may add little things

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css"
 import { useState } from "react"
 import Axios from "axios"
 import styled from "styled-components"
-
+import Dialog from "@mui/material/Dialog"
+import { DialogTitle } from "@mui/material"
+import { DialogActions } from "@mui/material"
+import { DialogContent } from "@mui/material"
 import {
   Header,
   AppIcon,
@@ -16,8 +19,6 @@ import {
 const APP_ID = "3eb6f612"
 
 const APP_KEY = "a8b42ab8cc62a2fabaf73db8260bf10f"
-
-
 
 const Container = styled.div`
   display: flex;
@@ -43,6 +44,7 @@ padding: 30px;
 `
 const CoverImage = styled.img`
   height: 200px;
+  object-fit: cover;
 `
 
 const RecipeaName = styled.span`
@@ -69,36 +71,75 @@ const SeeMoreText = styled(IngredientsText)`
   border: solid 1px #eb3300;
 `
 
+const Placeholder =  styled.img `
+width: 120px;
+margin: 200px;
+height: 200px;
+opacity: 50%;
+`
+
 const RecipeComponent = (props) => {
-  // const [show, setShow] = useState("")
+  const [show, setShow] = useState(false)
   console.log(props)
-  const {recipeObj} = props
+  const { recipeObj } = props
   return (
-<RecipeContainer>
-  <CoverImage src={recipeObj.image} />
-  <RecipeaName>{recipeObj.label}</RecipeaName>
-  <IngredientsText>{}</IngredientsText>
-  <SeeMoreText>See complete recipe</SeeMoreText>
-</RecipeContainer>  )
-
+    <>
+      <Dialog open={show}>
+        <DialogTitle id="Alert">Ingredients</DialogTitle>
+        <DialogContent>
+          <tabel>
+            <thead>
+              <th>Ingredients</th>
+              <th>Weight</th>
+            </thead>
+            <tbody>
+              {recipeObj.ingredients.map((ingredientsObj) => (
+                <tr>
+                  <td>{ingredientsObj.text}</td>
+                  <td>{ingredientsObj.weight}</td>
+                </tr>
+              ))}
+            </tbody>
+          </tabel>
+        </DialogContent>
+        <DialogActions>
+          <IngredientsText onClick={() => window.open(recipeObj.url)}>
+            See More{" "}
+          </IngredientsText>
+          <SeeMoreText onClick={() => setShow(false)}>Close</SeeMoreText>
+        </DialogActions>
+      </Dialog>
+      <RecipeContainer>
+        <CoverImage src={recipeObj.image} />
+        <RecipeaName>{recipeObj.label}</RecipeaName>
+        <IngredientsText onClick={() => setShow(true)}>
+          Ingredients
+        </IngredientsText>
+        <SeeMoreText onClick={() => window.open(recipeObj.url)}>
+          See complete recipe
+        </SeeMoreText>
+      </RecipeContainer>
+    </>
+  )
 }
-
 
 function App() {
   const [timeoutId, updateTimeoutId] = useState()
   const [recipeList, updateRecipeList] = useState([])
-const fetchRecipe = async  (searchString) => {
-  const response =  await Axios.get(
-    `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+  const fetchRecipe = async (searchString) => {
+    const response = await Axios.get(
+      `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    )
     console.log(response)
     updateRecipeList(response.data.hits)
-}
+  }
 
   const onTextChange = (e) => {
     clearTimeout(timeoutId)
     const timeout = setTimeout(() => fetchRecipe(e.target.value), 500)
     updateTimeoutId(timeout)
   }
+
   return (
     // <div className="App">
     <Container>
@@ -118,15 +159,16 @@ const fetchRecipe = async  (searchString) => {
         </SearchComponent>
       </Header>
       <RecipeListContainer>
-        {recipeList.length  && recipeList.map((recipeObj, index) => (
-          <RecipeComponent key = {index} recipeObj={recipeObj.recipe} />
-))}
-        <RecipeContainer>
+        {recipeList.length ?
+          recipeList.map((recipeObj, index) => (
+            <RecipeComponent key={index} recipeObj={recipeObj.recipe} />
+          )): <Placeholder src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Rpb_food_icon.svg/1200px-Rpb_food_icon.svg.png"></Placeholder> }
+        {/* <RecipeContainer>
           <CoverImage src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Rpb_food_icon.svg/1200px-Rpb_food_icon.svg.png" />
           <RecipeaName>MEtar paneer</RecipeaName>
           <IngredientsText> Ingrediemts</IngredientsText>
           <SeeMoreText>See complete reciple option </SeeMoreText>
-        </RecipeContainer>
+        </RecipeContainer> */}
       </RecipeListContainer>
     </Container>
 
